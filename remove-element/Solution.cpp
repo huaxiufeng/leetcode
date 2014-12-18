29ba655fb006b02d2a4eff9dd0b128dcb33f8592
huaxiufeng <huaxiufeng@kaisquare.com.cn>
2014-12-18 23:52:02 +0800

    add remove element

---
+++ remove-element/Solution.cpp
@@ -0,0 +1,22 @@
+/*
+Given an array and a value, remove all instances of that value in place and return the new length.
+The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+*/
+
+class Solution
+{
+public:
+    int removeElement(int A[], int n, int elem) {
+        int len = n;
+        for (int i = 0; i < len; i++) {
+            while (A[i] == elem) {
+                A[i] = A[len - 1];
+                len--;
+                if (len <= i) {
+                    return len;
+                }
+            }
+        }
+        return len;
+    }
+};
---
+++ remove-element/TestCase.cpp
@@ -0,0 +1,37 @@
+#define BOOST_TEST_MAIN
+#include <iostream>
+#include <boost/test/included/unit_test.hpp>
+#include <boost/assign.hpp>
+using namespace std;
+using namespace boost::assign;
+
+#include "Solution.cpp"
+
+struct global_fixture
+{
+    global_fixture()
+    {
+    }
+    ~global_fixture()
+    {
+    }
+};
+
+BOOST_GLOBAL_FIXTURE(global_fixture);
+struct assign_fixture
+{
+    assign_fixture()
+    {
+    }
+    ~assign_fixture()
+    {
+    }
+    Solution s;
+};
+
+BOOST_FIXTURE_TEST_SUITE(s_assign, assign_fixture)
+BOOST_AUTO_TEST_CASE(t_assign_1)
+{
+}
+
+BOOST_AUTO_TEST_SUITE_END()
