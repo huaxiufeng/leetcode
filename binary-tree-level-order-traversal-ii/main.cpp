062050eb3423c378f880aaecf0dc5d40de0516e9
huaxiufeng <huaxiufeng@kaisquare.com.cn>
2015-10-12 15:55:00 +0800

    add level order traversal binary tree

---
+++ binary-tree-level-order-traversal-ii/main.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <algorithm>
+using namespace std;
+
+template <typename T>
+void display(vector<T> array) {
+    for (int i = 0; i < array.size(); i++) {
+        cout<<array[i]<<" ";
+    }
+    cout<<endl;
+}
+
+template <typename T>
+void display(vector<vector<T>>& array) {
+    for (int i = 0; i < array.size(); i++) {
+        display(array[i]);
+    }
+}
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+void CreateBinaryTree(TreeNode*& root, const int* arr, int& index)
+{
+    if (arr[index] == -1) {
+        root = NULL;
+    } else {
+        root = new TreeNode(arr[index]);
+        CreateBinaryTree(root->left, arr, ++index);
+        CreateBinaryTree(root->right, arr, ++index);
+    }
+}
+
+void PreOderTraverse(TreeNode* root)
+{
+    if (root) {
+        cout<<root->val<<" ";
+        PreOderTraverse(root->left);
+        PreOderTraverse(root->right);
+    }
+}
+
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        vector<vector<int>> result;
+        if (!root) {
+            return result;
+        }
+        queue<TreeNode*> nodes;
+        nodes.push(root);
+        while (!nodes.empty()) {
+            vector<int> line;
+            queue<TreeNode*> next_nodes;
+            while (!nodes.empty()) {
+                line.push_back(nodes.front()->val);
+                if (nodes.front()->left) {
+                    next_nodes.push(nodes.front()->left);
+                }
+                if (nodes.front()->right) {
+                    next_nodes.push(nodes.front()->right);
+                }
+                nodes.pop();
+            }
+            result.push_back(line);
+            swap(next_nodes, nodes);
+        }
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
+
+/*
+        1
+    2       3
+  4   5   6   7
+*/
+
+int main() {
+    int arr[] = {1,2,4,-1,-1,5,-1,-1,3,6,-1,-1,7,-1,-1};
+    TreeNode* root = 0;
+    int index = 0;
+    CreateBinaryTree(root, arr, index);
+    PreOderTraverse(root);
+    cout<<endl;
+
+    Solution s;
+    vector<vector<int>> result = s.levelOrderBottom(root);
+    display(result);
+    return 0;
+}
+
