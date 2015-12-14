e4d6d46756c1874eafb0ac571c5773c11e86cd71
huaxiufeng <huaxiufeng@kaisquare.com.cn>
2015-12-14 18:36:45 +0800

    update permutations

--- permutations/main.cpp
+++ permutations/main.cpp
@@ -5,6 +5,22 @@
 #include <algorithm>
 using namespace std;
 
+template <typename T>
+void display(vector<T> array) {
+    for (int i = 0; i < array.size(); i++) {
+        cout<<array[i]<<" ";
+    }
+    cout<<endl;
+}
+
+template <typename T>
+void display(vector<vector<T>>& array) {
+    for (int i = 0; i < array.size(); i++) {
+        display(array[i]);
+    }
+}
+
+
 class Solution {
 public:
     void display(vector<int>& v) {
@@ -37,6 +53,7 @@ public:
         return res;
     }
 */
+/*
     vector< vector<int> > permute(vector<int>& nums) {
         cout<<"permuting ... ";
         display(nums);
@@ -60,13 +77,38 @@ public:
         }
         return res;
     }
+*/
+    void permuteImpl(vector<int>& nums, int index, vector<vector<int>>& result) {
+        if (index == nums.size()) {
+            result.push_back(nums);
+            return;
+        }
+
+        for (int i = index; i < nums.size(); i++) {
+            swap(nums[i], nums[index]);
+            permuteImpl(nums, index+1, result);
+            swap(nums[i], nums[index]);
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> res;
+        if (nums.size() == 1) {
+            res.push_back(nums);
+            return res;
+        }
+
+        permuteImpl(nums, 0, res);
+        return res;
+    }
 };
 
 int main() {
     int num[] = {1,2,3};
-    vector<int> nums(num, num+3);
+    vector<int> nums(num, num+sizeof(num)/sizeof(int));
     Solution s;
-    s.permute(nums);
+    vector<vector<int>> res = s.permute(nums);
+    display(res);
     return 0;
 }
 
