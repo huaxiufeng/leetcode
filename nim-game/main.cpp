905170acce287e58bd2537451bb0e0b1c27a2baf
huaxiufeng <huaxiufeng@kaisquare.com.cn>
2015-12-28 10:05:20 +0800

    add nim game

---
+++ nim-game/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+template <typename T>
+void display(vector<T> array) {
+    for (int i = 0; i < array.size(); i++) {
+        cout<<array[i]<<" ";
+    }
+    cout<<endl;
+}
+
+template <typename T>
+void display(vector<vector<T>>& array) {
+    for (int i = 0; i < array.size(); i++) {
+        display(array[i]);
+    }
+}
+
+class Solution {
+public:
+    bool canWinNim(int n) {
+        return n%4 != 0;
+    }
+};
+
+int main() {
+    Solution s;
+    cout<<s.canWinNim(9)<<endl;
+    return 0;
+}
+
