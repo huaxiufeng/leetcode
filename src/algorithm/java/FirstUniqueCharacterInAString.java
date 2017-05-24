551d76e25686f696259795b7f4e99b77eff77625
hust_feng <hust_feng@163.com>
2017-05-25 00:04:02 +0800

    FirstUniqueCharacterInAString

---
+++ src/algorithm/java/FirstUniqueCharacterInAString.java
@@ -0,0 +1,44 @@
+package algorithm.java;
+
+/**
+ * Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+
+ Examples:
+
+ s = "leetcode"
+ return 0.
+
+ s = "loveleetcode",
+ return 2.
+ Note: You may assume the string contain only lowercase letters.
+ * Created by amyhuiye on 2017/5/24.
+ */
+public class FirstUniqueCharacterInAString {
+
+    public int firstUniqChar(String s) {
+        if (null == s || 0 == s.length()) {
+            return -1;
+        }
+        if (1 == s.length()) {
+            return 0;
+        }
+        int[] counter = new int[256];
+        int len = s.length();
+        for (int i = 0; i < len; i++) {
+            counter[s.charAt(i)]++;
+        }
+        for (int i = 0; i < len; i++) {
+            if (1 == counter[s.charAt(i)]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        FirstUniqueCharacterInAString solution = new FirstUniqueCharacterInAString();
+        System.out.println(solution.firstUniqChar("leetcode"));
+        System.out.println(solution.firstUniqChar("loveleetcode"));
+        System.out.println(solution.firstUniqChar("aaaa"));
+    }
+}
