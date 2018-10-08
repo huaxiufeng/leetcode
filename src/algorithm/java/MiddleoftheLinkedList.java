1ab2d875af42ee94a69f42d2035f09380326074d
huaxiufeng <huaxiufeng@mucang.cn>
2018-10-08 22:43:53 +0800

    MiddleoftheLinkedList

---
+++ src/algorithm/java/MiddleoftheLinkedList.java
@@ -0,0 +1,25 @@
+package algorithm.java;
+
+import algorithm.java.data.ListNode;
+
+/**
+ * Created by huaxiufeng on 18/10/8.
+ */
+public class MiddleoftheLinkedList {
+
+    public ListNode middleNode(ListNode head) {
+        if (null == head || null == head.next) {
+            return head;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        if (fast.next != null) {
+            slow = slow.next;
+        }
+        return slow;
+    }
+}
