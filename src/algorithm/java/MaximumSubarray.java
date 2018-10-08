6fccf5b86cd524ec2b3fbca8e6b928ab8a87717f
huaxiufeng <huaxiufeng@mucang.cn>
2018-10-08 23:21:32 +0800

    MaximumSubarray

---
+++ src/algorithm/java/MaximumSubarray.java
@@ -0,0 +1,27 @@
+package algorithm.java;
+
+/**
+ * Created by huaxiufeng on 18/10/8.
+ */
+public class MaximumSubarray {
+
+    public int maxSubArray(int[] nums) {
+        if (null == nums || nums.length == 0) {
+            return 0;
+        }
+        int maxEnd = nums[0];
+        int result = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            maxEnd = Math.max(maxEnd + nums[i], nums[i]);
+            if (maxEnd > result) {
+                result = maxEnd;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        MaximumSubarray solution = new MaximumSubarray();
+        System.out.println(solution.maxSubArray(new int[]{-2,1,-3,4,-1,2,1,-5,4}));
+    }
+}
