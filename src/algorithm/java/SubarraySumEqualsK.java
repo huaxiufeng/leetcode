a180e3d8b7be3b68f635823665e361cd27585859
huaxiufeng <huaxiufeng@mucang.cn>
2018-10-13 16:52:59 +0800

    SubarraySumEqualsK

---
+++ src/algorithm/java/SubarraySumEqualsK.java
@@ -0,0 +1,60 @@
+package algorithm.java;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Given an array of integers and an integer k, you need to find the total number of continuous subarrays whose sum equals to k.
+
+ Example 1:
+ Input:nums = [1,1,1], k = 2
+ Output: 2
+ Note:
+ The length of the array is in range [1, 20,000].
+ The range of numbers in the array is [-1000, 1000] and the range of the integer k is [-1e7, 1e7].
+
+ * Created by huaxiufeng on 18/10/13.
+ */
+public class SubarraySumEqualsK {
+
+    public int subarraySum0(int[] nums, int k) {
+        if (null == nums || nums.length == 0) {
+            return 0;
+        }
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int sum = 0;
+            for (int j = i; j < nums.length; j++) {
+                sum += nums[j];
+                if (sum == k) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public int subarraySum(int[] nums, int k) {
+        if (null == nums || nums.length == 0) {
+            return 0;
+        }
+        int sum = 0, result = 0;
+        Map<Integer, Integer> preSum = new HashMap<>();
+        preSum.put(0, 1);
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (preSum.containsKey(sum - k)) {
+                result += preSum.get(sum - k);
+            }
+            preSum.put(sum, preSum.getOrDefault(sum, 0) + 1);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        SubarraySumEqualsK solution = new SubarraySumEqualsK();
+        System.out.println(solution.subarraySum(new int[]{1,1,1}, 2));
+    }
+}
