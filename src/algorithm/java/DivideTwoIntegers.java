7bdcdcac00d9fd105af3b0fe6d12e9780268d0a7
huaxiufeng <huaxiufeng@mucang.cn>
2018-11-03 17:10:46 +0800

    DivideTwoIntegers

---
+++ src/algorithm/java/DivideTwoIntegers.java
@@ -0,0 +1,42 @@
+package algorithm.java;
+
+/**
+ * Created by huaxiufeng on 18/11/3.
+ */
+public class DivideTwoIntegers {
+
+    public int divide(int dividend, int divisor) {
+        boolean negative = (dividend > 0) ^ (divisor > 0);
+        long lDividend = Math.abs((long)dividend);
+        long lDivisor = Math.abs((long)divisor);
+        long lRes = longDivide(lDividend, lDivisor);
+        lRes = negative ? (0 - lRes) : lRes;
+        if (lRes >= Integer.MAX_VALUE) {
+            return Integer.MAX_VALUE;
+        } if (lRes <= Integer.MIN_VALUE) {
+            return Integer.MIN_VALUE;
+        }
+        return (int)lRes;
+    }
+
+    private long longDivide(long dividend, long divisor) {
+        if (0 == dividend || dividend < divisor) {
+            return 0;
+        }
+        long sum = divisor;
+        long count = 1;
+        while (sum + sum < dividend) {
+            sum += sum;
+            count += count;
+        }
+        return count + longDivide(dividend - sum, divisor);
+    }
+
+    public static void main(String[] args) {
+        DivideTwoIntegers solution = new DivideTwoIntegers();
+        System.out.println(solution.divide(6, 3));
+        System.out.println(solution.divide(-2147483648, -1));
+        System.out.println(solution.divide(-7, 3));
+        System.out.println(solution.divide(1, 1));
+    }
+}
