739ce196415b9f9e707a4eab8dfeb7e472e8e98c
huaxiufeng <huaxiufeng@mucang.cn>
2018-11-16 11:45:52 +0800

    LC97InterleavingString

--- src/algorithm/java/LC97InterleavingString.java
+++ src/algorithm/java/LC97InterleavingString.java
@@ -73,8 +73,27 @@ public class LC97InterleavingString {
 //    }
 
     public boolean isInterleave(String s1, String s2, String s3) {
-        // TODO
-        return false;
+        if (null == s1 || null == s2 || null == s3) {
+            return false;
+        } else if (s1.length() + s2.length() != s3.length()) {
+            return false;
+        }
+        // dp[i][j]为true表示s1的前i个字符和s2的前j个字符交叉可以得到s3的前i+j个字符
+        boolean[][] dp = new boolean[s1.length() + 1][s2.length() + 1];
+        dp[0][0] = true;
+        for (int i = 1; i <= s1.length(); i++) {
+            dp[i][0] = dp[i-1][0] && (s1.charAt(i-1) == s3.charAt(i-1));
+        }
+        for (int j = 1; j <= s2.length(); j++) {
+            dp[0][j] = dp[0][j-1] && (s2.charAt(j-1) == s3.charAt(j-1));
+        }
+        for (int i = 1; i <= s1.length(); i++) {
+            for (int j = 1; j <= s2.length(); j++) {
+                dp[i][j] = (dp[i][j-1] && s2.charAt(j-1) == s3.charAt(i+j-1))
+                        || (dp[i-1][j] && s1.charAt(i-1) == s3.charAt(i+j-1));
+            }
+        }
+        return dp[s1.length()][s2.length()];
     }
 
     public static void main(String[] args) {
