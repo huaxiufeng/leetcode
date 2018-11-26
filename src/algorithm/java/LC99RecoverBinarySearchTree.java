e859def681613fccc68833a3209c848261597b8d
huaxiufeng <huaxiufeng@mucang.cn>
2018-11-26 19:37:03 +0800

    LC99RecoverBinarySearchTree

---
+++ src/algorithm/java/LC99RecoverBinarySearchTree.java
@@ -0,0 +1,18 @@
+package algorithm.java;
+
+import algorithm.java.data.TreeNode;
+
+/**
+ * Created by huaxiufeng on 18/11/26.
+ */
+public class LC99RecoverBinarySearchTree {
+
+    public void recoverTree(TreeNode root) {
+        // TODO
+    }
+
+    public static void main(String[] args) throws Exception {
+        LC99RecoverBinarySearchTree solution = new LC99RecoverBinarySearchTree();
+        solution.recoverTree(TreeNode.fromLevelSequence("[1,3,null,null,2,null,null]"));
+    }
+}
