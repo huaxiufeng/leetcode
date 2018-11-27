adadcc2057a78b7d1bea018b56038614d159a8f4
huaxiufeng <huaxiufeng@mucang.cn>
2018-11-27 16:50:55 +0800

    LC99RecoverBinarySearchTree

--- src/algorithm/java/LC99RecoverBinarySearchTree.java
+++ src/algorithm/java/LC99RecoverBinarySearchTree.java
@@ -7,8 +7,34 @@ import algorithm.java.data.TreeNode;
  */
 public class LC99RecoverBinarySearchTree {
 
+    private TreeNode preNode;
+    private TreeNode big;
+    private TreeNode small;
+
     public void recoverTree(TreeNode root) {
-        // TODO
+        inorderHelper(root);
+        if (big != null && small != null) {
+            int temp = big.val;
+            big.val = small.val;
+            small.val = temp;
+        }
+    }
+
+    private void inorderHelper(TreeNode root) {
+        if (null == root) {
+            return;
+        }
+        inorderHelper(root.left);
+
+        if (preNode != null && preNode.val > root.val) {
+            if (null == big) { // 只有一个大的是需要被交换的
+                big = preNode;
+            }
+            small = root;
+        }
+        preNode = root;
+
+        inorderHelper(root.right);
     }
 
     public static void main(String[] args) throws Exception {
