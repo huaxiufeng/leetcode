1f9f570156484f067d3f81071eb6947137f2b268
hust_feng <hust_feng@163.com>
2019-01-08 01:31:34 +0800

    LC73SetMatrixZeroes

--- src/algorithm/java/LC73SetMatrixZeroes.java
+++ src/algorithm/java/LC73SetMatrixZeroes.java
@@ -8,59 +8,55 @@ package algorithm.java;
 public class LC73SetMatrixZeroes {
 
     public void setZeroes(int[][] matrix) {
-        Boolean isCol = false;
-        int R = matrix.length;
-        int C = matrix[0].length;
-
-        for (int i = 0; i < R; i++) {
-
-            // Since first cell for both first row and first column is the same i.e. matrix[0][0]
-            // We can use an additional variable for either the first row/column.
-            // For this solution we are using an additional variable for the first column
-            // and using matrix[0][0] for the first row.
-            if (matrix[i][0] == 0) {
-                isCol = true;
-            }
-
-            for (int j = 1; j < C; j++) {
-                // If an element is zero, we set the first element of the corresponding row and column to 0
+        boolean firstRowHasZero = false;
+        boolean firstColumnHasZero = false;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
                 if (matrix[i][j] == 0) {
-                    matrix[0][j] = 0;
                     matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                    if (i == 0) {
+                        firstRowHasZero = true;
+                    }
+                    if (j == 0) {
+                        firstColumnHasZero = true;
+                    }
                 }
             }
         }
-
-        // Iterate over the array once again and using the first row and first column, update the elements.
-        for (int i = 1; i < R; i++) {
-            for (int j = 1; j < C; j++) {
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[i].length; j++) {
                 if (matrix[i][0] == 0 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
                 }
             }
         }
 
-        // See if the first row needs to be set to zero as well
-        if (matrix[0][0] == 0) {
-            for (int j = 0; j < C; j++) {
-                matrix[0][j] = 0;
+        if (firstColumnHasZero) {
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[i][0] = 0;
             }
         }
-
-        // See if the first column needs to be set to zero as well
-        if (isCol) {
-            for (int i = 0; i < R; i++) {
-                matrix[i][0] = 0;
+        if (firstRowHasZero) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[0][j] = 0;
             }
         }
     }
 
     public static void main(String[] args) {
         LC73SetMatrixZeroes solution = new LC73SetMatrixZeroes();
-        solution.setZeroes(new int[][]{
+        int[][] matrix = new int[][]{
                 {0, 1, 2, 0},
                 {3, 4, 5, 2},
                 {1, 3, 1, 5}
-        });
+        };
+        solution.setZeroes(matrix);
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println("");
+        }
     }
 }
