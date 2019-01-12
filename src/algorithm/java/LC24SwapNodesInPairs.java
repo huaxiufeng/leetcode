5d2bbda0b098ff75ff8d15442505fd8b1ac5993b
huaxiufeng <huaxiufeng@mucang.cn>
2019-01-12 13:57:09 +0800

    LC24SwapNodesInPairs

--- src/algorithm/java/LC24SwapNodesInPairs.java
+++ src/algorithm/java/LC24SwapNodesInPairs.java
@@ -5,7 +5,7 @@ import algorithm.java.data.ListNode;
 /**
  * Created by huaxiufeng on 18/11/3.
  */
-public class SwapNodesInPairs {
+public class LC24SwapNodesInPairs {
 
     public ListNode swapPairs(ListNode head) {
         if (null == head || head.next == null) {
@@ -19,7 +19,7 @@ public class SwapNodesInPairs {
     }
 
     public static void main(String[] args) {
-        SwapNodesInPairs solution = new SwapNodesInPairs();
+        LC24SwapNodesInPairs solution = new LC24SwapNodesInPairs();
         System.out.println(solution.swapPairs(ListNode.fromVals(1,2,3,4)).toString());
     }
 }
