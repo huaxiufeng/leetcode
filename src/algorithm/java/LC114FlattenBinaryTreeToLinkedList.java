f4e906a0c0137c481dcad1d6726d8b2ef30fc904
hust_feng <hust_feng@163.com>
2019-08-06 16:36:35 +0800

    LC114FlattenBinaryTreeToLinkedList

--- src/algorithm/java/LC114FlattenBinaryTreeToLinkedList.java
+++ src/algorithm/java/LC114FlattenBinaryTreeToLinkedList.java
@@ -7,7 +7,7 @@ import algorithm.java.data.TreeNode;
  */
 public class LC114FlattenBinaryTreeToLinkedList {
 
-    public void flatten(TreeNode root) {
+    public void flatten0(TreeNode root) {
         if (null == root) {
             return;
         }
@@ -22,5 +22,35 @@ public class LC114FlattenBinaryTreeToLinkedList {
             cursor = cursor.right;
         }
         cursor.right = right;
+    }
+
+    public void flatten(TreeNode root) {
+        doFlatten(root);
+    }
+
+    private TreeNode[] doFlatten(TreeNode root) {
+        if (null == root) {
+            return new TreeNode[]{null, null};
+        }
+        TreeNode[] resLeft = doFlatten(root.left);
+        TreeNode[] resRight = doFlatten(root.right);
+        root.left = null;
+        TreeNode tail = root;
+        if (resLeft[0] != null && resLeft[1] != null) {
+            tail.right = resLeft[0];
+            tail = resLeft[1];
+        }
+        if (resRight[0] != null && resRight[1] != null) {
+            tail.right = resRight[0];
+            tail = resRight[1];
+        }
+        return new TreeNode[]{root, tail};
+    }
+
+    public static void main(String[] args) throws Exception {
+        TreeNode root = TreeNode.fromLevelSequence("[1,2,5,3,4,null,6]");
+        LC114FlattenBinaryTreeToLinkedList solution = new LC114FlattenBinaryTreeToLinkedList();
+        solution.flatten(root);
+        System.out.println(root.toString());
     }
 }
