f83ab665bce2f54b90f2a3edd3d0b717f657c2cb
hust_feng <hust_feng@163.com>
2019-08-22 16:07:28 +0800

    水塘抽样

--- src/algorithm/java/LC382LinkedListRandomNode.java
+++ src/algorithm/java/LC382LinkedListRandomNode.java
@@ -12,9 +12,12 @@ import algorithm.java.data.ListNode;
  Follow up:
  What if the linked list is extremely large and its length is unknown to you? Could you solve this efficiently without using extra space?
 
- http://blog.jobbole.com/42550/
- 假设当前正要读取第n个数据，则我们以1/n的概率留下该数据，否则留下前n-1个数据中的一个。
- 以这种方法选择，所有数据流中数据被选择的概率一样。
+ https://www.cnblogs.com/strugglion/p/6424874.html
+ 读取第1个数据的时候，保留该数据;
+ 读取第2个数据的时候，以1/2的概率替换之前的数据，1/2的概率保留之前的数据;
+ 读取第3个数据的时候，以1/3的概率替换之前的数据，2/3的概率保留之前的数据;
+ 之后读取第n个数据，则我们以1/n的概率替换之前的数据，否则留下之前的数据。
+ 以这种方法选择，所有数据流中数据被选择的概率一样，都是1/n。
  简短的证明：假设n-1时候成立，即前n-1个数据被返回的概率都是1/n-1,当前正在读取第n个数据，以1/n的概率返回它。
  那么前n-1个数据中数据被返回的概率为：(1/(n-1))*((n-1)/n)= 1/n，
 
